0c27c36e1ea4417079d3733040e5a669089f5e2a
Kauê Gatto <kauegatto123@gmail.com>
2020-12-07 16:56:16 -0300

    Testing EFCore on MySQL using dbFirst; Made EFCORE connect to mysql; Now EFCore retrieves data and can a new object (Command); Warning concurrency conflicts; EfCore tests were made on program.cs to try the commands, those tests will be implemented within the RestAPI

--- CommandAPI/Models/Command.cs
+++ CommandAPI/Models/Command.cs
@@ -1,9 +1,19 @@
 namespace CommandAPI.Models{
     public class Command{
+        #region constructors
+
+        public Command(string _howTo, string _platform, string _commandLine){
+            this.ds_howTo = _howTo;
+            this.nm_platform = _platform;
+            this.ds_commandLine = _commandLine;
+        }
+        public Command(){
+        }
+        #endregion
         public int id {get;set;}
-        public string howTo{get;set;}
-        public string platform{get;set;}
-        public string commandLine{get;set;}
+        public string ds_howTo{get;set;}
+        public string nm_platform{get;set;}
+        public string ds_commandLine{get;set;}
         
     }
 }
--- CommandAPI/Program.cs
+++ CommandAPI/Program.cs
@@ -15,10 +15,57 @@ namespace CommandAPI
     {
         public static void Main(string[] args)
         {   
-            var context = new CommandContext();
-            var commandAmount = context.command.Count();
-            Console.WriteLine(commandAmount); 
-            CreateWebHostBuilder(args).Build().Run();
+            using (var context = new CommandContext())
+            {
+                #region Read
+                        Console.WriteLine("Retrieving  all commands on the db");
+                        List<Command> commandlist = context.command.OrderBy(c => c.id).ToList();
+                        Console.WriteLine("Done! commands: ");
+                        int cmdAmount = commandlist.Count;
+                        for (int i = 0; i < cmdAmount; i++)
+                        {
+                            Console.WriteLine("Id: "+commandlist[i].id +"; CommandLine: "+commandlist[i].ds_commandLine);   
+                        }
+
+                        Console.WriteLine("Counting the amount of commands in db");
+                        Console.WriteLine("Done! amount : " + cmdAmount); 
+
+                    #endregion
+                #region Create
+                        Console.WriteLine("Adding a new command to the db...");
+                        var newCommand = new Command
+                        {
+                            ds_howTo = "Open the cmd and execute it with enter",
+                            nm_platform ="Windows git",
+                            ds_commandLine = "git add ."
+                        };
+                        context.Add(newCommand);
+                        Console.WriteLine("Added " + newCommand +"!");
+                        
+                    #endregion
+                Console.WriteLine("Saving changes...");
+                try{
+                    context.SaveChanges();
+                    Console.Write("Changes saved!");                        
+                }
+                catch (Exception e){
+                    Console.WriteLine("Concurrency conflict");
+                    Console.WriteLine(e.ToString().Substring(0,255)+"...");
+                }
+                #region Read (here for testing, not going to be implemented)
+                    Console.WriteLine("Retrieving  all commands on the db");
+                    List<Command> commandlist2 = context.command.OrderBy(c => c.id).ToList();
+                    Console.WriteLine("Done! commands: ");
+                    int cmdAmount2 = commandlist2.Count;
+                    for (int i = 0; i < cmdAmount2; i++)
+                    {
+                            Console.WriteLine("Id: "+commandlist2[i].id +"; CommandLine: "+commandlist2[i].ds_commandLine);   
+                    }
+                    Console.WriteLine("Counting the amount of commands in db");
+                    Console.WriteLine("Done! amount : " + cmdAmount2); 
+                #endregion
+                //CreateWebHostBuilder(args).Build().Run();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
